666fa41da867355d4184957e735c98cf9eff18b8
HumzaAslam2 <125995467+HumzaAslam2@users.noreply.github.com>
2023-03-27 13:00:11 -0500

    Added username to the Main scene
    Added code to allow username input to be displayed next to the high score.

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField usernameInput;
+    [SerializeField] private Button startButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        startButton.onClick.AddListener(SetUsername);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void SetUsername()
+    {
+        GameManager.Instance.username = usernameInput.text;
+    }
+}
